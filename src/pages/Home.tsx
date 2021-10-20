3d2a784e554f98d1d75515e3c4eab411c77fa6bf
Nico Neiman <niconeiman@gmail.com>
2021-10-20 12:14:10 +0300

    ionic

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,25 +1,52 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
-
+import {
+	IonContent,
+	IonHeader,
+	IonList,
+	IonItem,
+	IonPage,
+	IonTitle,
+	IonToolbar,
+	IonNote,
+	IonButtons,
+	IonButton,
+	IonIcon,
+} from "@ionic/react";
+import ExploreContainer from "../components/ExploreContainer";
+import "./Home.css";
+import { useState } from "react";
+import { add } from "ionicons/icons";
 const Home: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
-      </IonContent>
-    </IonPage>
-  );
+	const [state, setstate] = useState([1, 2, 3]);
+	return (
+		<IonPage>
+			<IonHeader>
+				<IonToolbar>
+					<IonTitle>Esparragus</IonTitle>
+					<IonButtons>
+						<IonButton type='button'>
+							<IonIcon slot='start' icon={add} size={"small"}></IonIcon>
+							Add
+						</IonButton>
+					</IonButtons>
+				</IonToolbar>
+			</IonHeader>
+			<IonContent fullscreen>
+				<IonHeader collapse='condense'>
+					<IonToolbar>
+						<IonTitle size='large'>Is delicious</IonTitle>
+					</IonToolbar>
+				</IonHeader>
+				<IonList>
+					{state.map((item, index) => (
+						<IonItem key={index}>
+							{item}
+							<IonNote slot='end'>{Date.now()}</IonNote>
+						</IonItem>
+					))}
+				</IonList>
+			</IonContent>
+		</IonPage>
+	);
 };
 
 export default Home;
